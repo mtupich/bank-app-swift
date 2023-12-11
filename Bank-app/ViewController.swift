51afcb9c510afd9baf46132a8f706a7b3fad3357
Maria <mariatupich@gmail.com>
2023-12-11 14:42:05 -0300

    Initial Commit

---
+++ Bank-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank-app
+//
+//  Created by Maria Tupich on 11/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

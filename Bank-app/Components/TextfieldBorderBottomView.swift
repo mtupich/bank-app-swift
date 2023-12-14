573bce8c1fc5a64f47c5a067da149e12a07d15e7
Maria <mariatupich@gmail.com>
2023-12-14 14:19:43 -0300

    criando a interface de login

---
+++ Bank-app/Components/TextfieldBorderBottomView.swift
@@ -0,0 +1,84 @@
+//
+//  TextfieldBorderBottomView.swift
+//  Bank-app
+//
+//  Created by Maria Tupich on 14/12/23.
+//
+
+import Foundation
+import UIKit
+
+class TextfieldBorderBottomView: UIView {
+    
+    lazy var container: UIView = {
+        let container = UIView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
+    }()
+
+    lazy var title: UILabel = {
+        let title = UILabel()
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.text = "SENHA"
+        title.textColor = .white
+        title.font = UIFont.boldSystemFont(ofSize: 10)
+        return title
+    }()
+    
+    lazy var placeholder: UITextField = {
+       let tf = UITextField()
+       tf.translatesAutoresizingMaskIntoConstraints = false
+       tf.placeholder = "SENHA"
+       return tf
+    }()
+    
+    lazy var border: UIView = {
+       let border = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 1))
+       border.translatesAutoresizingMaskIntoConstraints = false
+       border.backgroundColor = .white
+       border.clipsToBounds = true
+       return border
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+        setUpConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func addSubviews() {
+        addSubview(container)
+        container.addSubview(title)
+        container.addSubview(placeholder)
+        container.addSubview(border)
+    }
+
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            container.topAnchor.constraint(equalTo: self.topAnchor),
+            container.leftAnchor.constraint(equalTo: self.leftAnchor),
+            container.rightAnchor.constraint(equalTo: self.rightAnchor),
+            container.heightAnchor.constraint(equalToConstant: 50),
+            
+            title.topAnchor.constraint(equalTo: container.topAnchor),
+            title.leftAnchor.constraint(equalTo: container.leftAnchor),
+            title.rightAnchor.constraint(equalTo: container.rightAnchor),
+            title.bottomAnchor.constraint(equalTo: placeholder.topAnchor),
+
+            placeholder.topAnchor.constraint(equalTo: title.bottomAnchor),
+            placeholder.leftAnchor.constraint(equalTo: container.leftAnchor),
+            placeholder.rightAnchor.constraint(equalTo: container.rightAnchor),
+            placeholder.bottomAnchor.constraint(equalTo: border.topAnchor),
+
+            border.topAnchor.constraint(equalTo: placeholder.bottomAnchor),
+            border.leftAnchor.constraint(equalTo: container.leftAnchor),
+            border.rightAnchor.constraint(equalTo: container.rightAnchor),
+            border.heightAnchor.constraint(equalToConstant: 1),
+        ])
+    }
+
+}
--- Bank-app/Login/LoginScreen.swift
+++ Bank-app/Login/LoginScreen.swift
@@ -22,34 +22,48 @@ class LoginScreen: UIView {
     lazy var loginContainer: UIView = {
        let container = UIView()
        container.translatesAutoresizingMaskIntoConstraints = false
-       container.backgroundColor = .white
+       container.backgroundColor = .clear
+       container.layer.cornerRadius = 8
+       container.layer.borderColor = UIColor.white.cgColor
+       container.layer.borderWidth = 1
        return container
     }()
     
     lazy var title: UILabel = {
         let title = UILabel()
         title.translatesAutoresizingMaskIntoConstraints = false
-        title.text = "Faça seu login"
+        title.text = "FAÇA SEU LOGIN"
+        title.font = .systemFont(ofSize: 16, weight: .bold)
+        title.textColor = .white
         return title
     }()
+ 
+    lazy var credentialView: TextfieldBorderBottomView = {
+       let tf = TextfieldBorderBottomView()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.title.text = "CPF"
+        tf.placeholder.placeholder = "CPF"
+       return tf
+    }()
     
-    lazy var loginStackView: UIStackView = {
-      let stack = UIStackView()
-      stack.translatesAutoresizingMaskIntoConstraints = false
-      stack.axis = .vertical
-      stack.alignment = .fill
-      stack.distribution = .fillEqually
-      stack.backgroundColor = .red
-      return stack
+    lazy var passwordView: TextfieldBorderBottomView = {
+       let tf = TextfieldBorderBottomView()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.title.text = "SENHA"
+        tf.placeholder.placeholder = "SENHA"
+       return tf
     }()
     
     lazy var loginBtn: UIButton = {
        let btn = UIButton()
        btn.translatesAutoresizingMaskIntoConstraints = false
-       btn.backgroundColor = .blue
        btn.setTitle("Entrar", for: .normal)
+       btn.layer.cornerRadius = 8
+       btn.layer.borderColor = UIColor.white.cgColor
+       btn.layer.borderWidth = 1
        return btn
     }()
+    
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -65,7 +79,8 @@ class LoginScreen: UIView {
         self.addSubview(self.bg)
         bg.addSubview(loginContainer)
         loginContainer.addSubview(title)
-        loginContainer.addSubview(loginStackView)
+        loginContainer.addSubview(credentialView)
+        loginContainer.addSubview(passwordView)
         loginContainer.addSubview(loginBtn)
     }
 
@@ -79,23 +94,23 @@ class LoginScreen: UIView {
             loginContainer.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             loginContainer.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             loginContainer.widthAnchor.constraint(equalToConstant: 280),
-            loginContainer.heightAnchor.constraint(equalToConstant: 300),
+            loginContainer.heightAnchor.constraint(equalToConstant: 260),
             
             title.topAnchor.constraint(equalTo: loginContainer.topAnchor, constant: 10),
             title.centerXAnchor.constraint(equalTo: loginContainer.centerXAnchor),
-            title.bottomAnchor.constraint(equalTo: loginStackView.topAnchor),
             
-            loginStackView.topAnchor.constraint(equalTo: title.bottomAnchor),
-            loginStackView.leftAnchor.constraint(equalTo: loginContainer.leftAnchor, constant: 24),
-            loginStackView.rightAnchor.constraint(equalTo: loginContainer.rightAnchor, constant: -24),
-            loginStackView.heightAnchor.constraint(equalToConstant: 140),
+            credentialView.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 20),
+            credentialView.leftAnchor.constraint(equalTo: loginContainer.leftAnchor, constant: 24),
+            credentialView.rightAnchor.constraint(equalTo: loginContainer.rightAnchor, constant: -24),
+            
+            passwordView.topAnchor.constraint(equalTo: credentialView.bottomAnchor, constant: 50),
+            passwordView.leftAnchor.constraint(equalTo: loginContainer.leftAnchor, constant: 24),
+            passwordView.rightAnchor.constraint(equalTo: loginContainer.rightAnchor, constant: -24),
 
-            loginBtn.topAnchor.constraint(equalTo: loginStackView.bottomAnchor, constant: 20),
-            loginBtn.centerXAnchor.constraint(equalTo: loginContainer.centerXAnchor),
+//            loginBtn.centerXAnchor.constraint(equalTo: loginContainer.centerXAnchor),
             loginBtn.leftAnchor.constraint(equalTo: loginContainer.leftAnchor, constant: 24),
             loginBtn.rightAnchor.constraint(equalTo: loginContainer.rightAnchor, constant: -24),
-
-
+            loginBtn.bottomAnchor.constraint(equalTo: loginContainer.bottomAnchor, constant: -24),
         ])
     }
 
